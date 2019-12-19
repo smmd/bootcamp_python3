8b873aae4001ac99dc57645493a442666dced3c7
Sagrario Meneses <sagrario.meneses@linio.com>
2019-12-19 16:23:00 -0600

    [WIP] Tarea 2 semana 2

--- smmd/semana2/02_tarea_prueba_tecnica.py
+++ smmd/semana2/02_tarea_prueba_tecnica.py
@@ -1,18 +1,33 @@
 #WIP
 import csv, json
+from datetime import datetime
 
-datos = {}
-with open('02_tarea.csv') as archivo:
-	lector = csv.reader(archivo, delimiter=',')
+def generarReporte():
+	datos = {
+		"fecha": datetime.now().strftime('%m/%d/%y'),
+		"total_efectivo": 0,
+		"total_acciones": 0,
+        "personas": [],
+	}
 
-	lineas = 0
+	with open('02_tarea.csv') as archivo:
+		lector = csv.DictReader(archivo, delimiter=',')
 
-	for linea in lector:
-		if lineas == 0:
-			continue
+		lineas = 0
+		for linea in lector:
+			if lineas == 0:
+				lineas += 1
+			else:
+				datos['total_efectivo'] += float(linea['EFECTIVO'])
+				datos['total_acciones'] += float(linea['ACCIONES'])
+				datos['personas'].append({
+                    'nombre': f'{linea["PERSONA"]}',
+                    'acciones': f'{linea["ACCIONES"]}',
+                    'efectivo': f'{linea["EFECTIVO"]}',
+                    'porcentaje': 'aiññ no quiero hacer otro for'
+                })
 			lineas += 1
-		else:
-			accion = int(linea[1])
-			datos["total_acciones"] += accion
 
-print(datos) 
+	return json.dumps(datos, indent=2)
+
+print(generarReporte())
