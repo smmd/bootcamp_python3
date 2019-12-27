9671896ed6a47ef79d9d683b6ddcf764962cc420
Sagrario Meneses <sagrario.meneses@linio.com>
2019-12-27 13:36:32 -0600

    Tarea 2 semana 3

---
+++ smmd/semana3/02_tarea_regex.py
@@ -0,0 +1,13 @@
+import re
+
+def es_rfc_valido(rfc):
+	if (re.search(r'[A-Z]{4}\d{6}[A-Z0-9]{3}$', rfc)):
+		return f'El RFC {rfc} es valido'
+	else:
+		return 'No se armo'
+
+
+print(es_rfc_valido('MELM8305281H0'))
+print(es_rfc_valido('MELM8328051H0'))
+print(es_rfc_valido('M3LM8305281H0'))
+print(es_rfc_valido('MELM8305281H0LA'))
